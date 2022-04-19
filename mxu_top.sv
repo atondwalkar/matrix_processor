56434fba2b1c26117818df046d4f3ebce3551987
“Ashish” <atondwalstudio@gmail.com>
2022-04-18 23:47:17 -0700

    working on mxu top interface

--- mxu_top.sv
+++ mxu_top.sv
@@ -16,23 +16,27 @@ module mxu_top(
     
     input logic clk, reset;
     
-    input logic awready, awvalid;
+    input logic awvalid;
+    output logic awready;
     input logic [15:0] awaddr;
     
     input logic [3:0] wstrb;
     input logic [31:0] wdata;
-    input logic wvalid, wready;
+    input logic wvalid;
+    output logic wready;
     
     logic load_en, mult_en, acc_en;
     
-    logic [7:0] cache [SIZE*SIZE*2+6:0];
-    // 0 - start
-    // 1 - done
-    // 2 - cycles
-    // 3 - memsel
-    // 4 - awaddr upper
-    // 5 - awaddr lower
-    // 6 - start of data
+    logic [7:0] cache [$clog2(SIZE*SIZE*2)-1:0];
+    //needs start, done, memsel, output
+    
+    logic [7:0] control_reg [1:0];
+    // 0 - awaddr upper
+    // 1 - awaddr lower
+    
+    logic strb_sel;
+    logic data_addr;
+    logic [7:0] data_data;
     
     control #(.SIZE(SIZE)) control_inst (
         .clk(clk),
@@ -52,15 +56,46 @@ module mxu_top(
     
     always @ (clk)
     begin
-        if(awvalid && awready)
+        if(awvalid)
         begin
-            cache[4] <= awaddr[15:8];
-            cache[5] <= awaddr[7:0];
+            awready <= 1;
+        end
+        else if (awvalid && awready)
+        begin
+            awready <= 0;
+            control_reg[0] <= awaddr[15:8];
+            control_reg[1] <= awaddr[7:0];
         end
         else
         begin
-            cache[4] <= cache[4];
-            cache[5] <= cache[5];
+            control_reg[0] <= control_reg[0];
+            control_reg[1] <= control_reg[1];
+        end
+    end
+   
+    assign data_addr = {control_reg[0][1:0], control_reg[1]};
+    
+    always @ (clk)
+    begin
+        if(wvalid)
+        begin
+            wready <= 1;
+            casez (wstrb)
+                4'b1??? : data_data <= wdata[31:24];
+                4'b01?? : data_data <= wdata[23:16];
+                4'b001? : data_data <= wdata[15:8];
+                4'b0001 : data_data <= wdata[7:0];
+                default : data_data <= 8'b1000_0001;
+            endcase
+        end
+        else if (wvalid && wready)
+        begin
+            wready <= 0;
+            cache[data_addr] <= data_data;
+        end
+        else
+        begin
+            cache[data_addr] <= data_data;
         end
     end
     
