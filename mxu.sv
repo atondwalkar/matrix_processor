56ad2f987576c5e56687de1b86e43f92152c3b22
“Ashish” <atondwalstudio@gmail.com>
2022-05-08 12:52:48 -0700

    changes for axi wrapper

--- mxu.sv
+++ mxu.sv
@@ -3,22 +3,33 @@
 module mxu(
     clk,
     reset,
-    wdata,
-    awaddr,
-    wready,
+    //wdata,
+    //awaddr,
+    //wready,
     rdata,
     araddr,
-    arready
+    //arready,
+    done_o,
+    start_in,
+    cycles_in,
+    data_a_in,
+    data_b_in
 );
 
     parameter SIZE = 4;
+    parameter BYTES = 4;
 
     input logic clk, reset;
 
-    input logic [31:0] awaddr;
-    input logic [8:0] wdata;
-    input logic wready;
-    input logic arready;
+    //input logic [31:0] awaddr;
+    //input logic [8:0] wdata;
+    //input logic wready;
+    //input logic arready;
+
+    input logic [SIZE*SIZE*8-1:0] data_a_in;
+    input logic [SIZE*SIZE*8-1:0] data_b_in;
+    input logic start_in, cycles_in;
+    output logic done_o;
 
     input logic [31:0] araddr;
     output logic [31:0] rdata;
@@ -28,7 +39,8 @@ module mxu(
     logic load_en, mult_en, acc_en;
     logic [SIZE-1:0] memsel;
 
-    logic [SIZE*SIZE*2 + 1:0][7:0] cache;
+
+    //logic [SIZE*SIZE*2 + 1:0][7:0] cache;
     // location 0 start will be 8'b0000_0001
     // location 0 done will be 8'b0000_0010
     // location 1 will be cycles in
@@ -55,8 +67,10 @@ module mxu(
             begin
                 for(y=0; y<SIZE; y=y+1)
                     begin
-                        data_plex_a[x][y] <= cache[x*SIZE+y + 2];
-                        data_plex_b[x][y] <= cache[y*SIZE+x + SIZE*SIZE + 2];
+                        //data_plex_a[x][y] <= cache[x*SIZE+y + 2];
+                        //data_plex_b[x][y] <= cache[y*SIZE+x + SIZE*SIZE + 2];
+                        data_plex_a[x][y] <= data_a_in[x*8*SIZE+y*8 +: 8];
+                        data_plex_b[x][y] <= data_b_in[y*8*SIZE+x*8 +: 8];
                     end
             end
     end
@@ -91,15 +105,19 @@ module mxu(
     control #(.SIZE(SIZE)) control_inst (
         .clk(clk),
         .reset(reset),
-        .cycles_in(cache[1]),
+        //.cycles_in(cache[1]),
+        .cycles_in(cycles_in),
         .load_en(load_en),
         .mult_en(mult_en),
         .acc_en(acc_en),
         .done(done),
-        .start(cache[0][0]),
+        //.start(cache[0][0]),
+        .start(start_in),
         .memsel(memsel),
         .next(next)
     );
+    
+    assign done_o = done;
 
     //systolic array instantiation
     array #(.SIZE(SIZE)) array_inst (
@@ -114,35 +132,40 @@ module mxu(
         .b_in(b_in)
     );
 
-    //read data address
-    always @ (posedge clk)
-    begin
-        if(arready)
-        begin
-            ar_bus <= araddr;
-            select <= araddr[$clog2(SIZE*SIZE)-1:0] - 1;
-        end
-    end
-    
-    //if read address is something, get accumulator data, else get status
-    assign rdata = ar_bus ? d_out : {24'b0, cache[0]};
 
-    //writing data to cache
-    always @ (posedge clk)
-    begin
-        if(done)
-            begin
-                cache[0] <= 8'b0000_0010;
-            end
-        else if(wready)
-            begin
-                cache[awaddr[$clog2(SIZE*SIZE*2 + 2):0]] <= wdata;
-            end
-        else
-            begin
-                cache[awaddr[$clog2(SIZE*SIZE*2 + 2):0]] <= cache[awaddr[$clog2(SIZE*SIZE*2 + 2):0]];
-            end
-    end
+    assign select = araddr[$clog2(SIZE*SIZE)-1:0] - 1;
+    assign rdata = d_out;
+
+
+//    //read data address
+//    always @ (posedge clk)
+//    begin
+//        if(arready)
+//        begin
+//            ar_bus <= araddr;
+//            select <= araddr[$clog2(SIZE*SIZE)-1:0] - 1;
+//        end
+//    end
+    
+//    //if read address is something, get accumulator data, else get status
+//    assign rdata = ar_bus ? d_out : {24'b0, cache[0]};
+
+//    //writing data to cache
+//    always @ (posedge clk)
+//    begin
+//        if(done)
+//            begin
+//                cache[0] <= 8'b0000_0010;
+//            end
+//        else if(wready)
+//            begin
+//                cache[awaddr[$clog2(SIZE*SIZE*2 + 2):0]] <= wdata;
+//            end
+//        else
+//            begin
+//                cache[awaddr[$clog2(SIZE*SIZE*2 + 2):0]] <= cache[awaddr[$clog2(SIZE*SIZE*2 + 2):0]];
+//            end
+//    end
 
 
 endmodule
